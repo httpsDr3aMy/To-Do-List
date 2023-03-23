3b461cf810b33000a21a15c1549ed0a7d3b24184
Kamil <125757644+httpsDr3aMy@users.noreply.github.com>
2023-03-23 20:36:06 +0100

    Update todolistsqlite.py

--- todolistsqlite.py
+++ todolistsqlite.py
@@ -1,44 +1,30 @@
-import datetime, sqlite3
+import datetime
+import sqlite3
 
-#making database
+DB_PATH = 'todolistsqllite/todolist.db'
+
+# make database and return connection object
 def make_db():
-    try:
-        connection = sqlite3.connect('todolistsqllite/todolist.db')
-        cur = connection.cursor()
-        cur.execute("""CREATE TABLE task(task text, current_time text, execution_time text)""")
-    except Exception:
-        pass
+    connection = sqlite3.connect(DB_PATH)
+    cur = connection.cursor()
+    cur.execute("""CREATE TABLE IF NOT EXISTS task(task TEXT, current_time TEXT, execution_time TEXT)""")
     return connection
 
-connection = make_db()
-
-#getting current time to print date of giving task
+# get current time in required format
 def getting_current_time():
     current_time_func = datetime.datetime.now()
-    day = current_time_func.day
-    month = current_time_func.month
-    year = current_time_func.year
-    hour = current_time_func.hour
-    minutes = current_time_func.minute
-    current_time = str(hour)+':'+str(minutes)+' '+str(year)+'-'+str(month)+'-'+str(day)
+    current_time = current_time_func.strftime('%H:%M %Y-%m-%d')
     return current_time
 
-#selecting all records
+# select all records from the database
 def select_records(connection):
     cur = connection.cursor()
     cur.execute("""SELECT * FROM task""")
     result = cur.fetchall()
     for row in result:
-        print(
-            f'\nZadanie: {str(row[0])}'
-            + '\nData wyslania zadania: '
-            + str(row[1])
-            + '\nData wykonania zadania: '
-            + str(row[2])
-            + '\n'
-        )
+        print(f'\nZadanie: {row[0]}\nData wyslania zadania: {row[1]}\nData wykonania zadania: {row[2]}\n')
 
-#inserting values to database
+# insert values into the database
 def inserting_values(connection, current_time):
     task = input('Jakie zadanie chcesz dodac? ')
     execution_time = input('Kiedy ma zostać zrealizowane (od której do której) (format: 16:39-18:00 2023-3-21): ')
@@ -46,7 +32,7 @@ def inserting_values(connection, current_time):
     cur.execute("""INSERT INTO task(task, current_time, execution_time) VALUES(?, ?, ?)""",(task, current_time, execution_time,))
     connection.commit()
 
-#deleting rows which user selected
+# delete row from the database
 def delete_row(connection):
     user_answer = int(input('\nJakiego zadania chcesz sie pozbyc? '))
     cur = connection.cursor()
